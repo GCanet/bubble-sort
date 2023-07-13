e994b1e888710ccbac9f72caff64c5274c3f507f
Gerard <kanetpwnz@gmail.com>
2023-07-13 17:05:30 +0200

    ruby style edit

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,16 +3,16 @@ bubblesort = [4,3,78,2,0,2]
 def bubble_sort(bubblesort)
   bubblesort_largo = bubblesort.size
   return bubblesort if bubblesort_largo <= 1
-  
-  loop do
+
+    loop do
     cambiado = false
-    (bubblesort_largo-1).times do |indiceACTUAL|
+    (bubblesort_largo - 1).times do |indiceACTUAL|
       if bubblesort[indiceACTUAL] > bubblesort[indiceACTUAL+1]
         bubblesort[indiceACTUAL], bubblesort[indiceACTUAL+1] = bubblesort[indiceACTUAL+1], bubblesort[indiceACTUAL]
         cambiado = true
       end
     end
     break if not cambiado
-  end
-  bubblesort  
+    end
+  bubblesort
 end
